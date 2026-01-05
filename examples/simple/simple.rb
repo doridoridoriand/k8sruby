9a9cd3b9d2d217007a60d5aa03b958c87de60f5b
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2026-01-05 18:55:42 +0900

    Rename namespace of simple.rb

--- examples/simple/simple.rb
+++ examples/simple/simple.rb
@@ -5,4 +5,4 @@ config = Kubernetes::Configuration.default_config
 Kubernetes.load_kube_config(ENV['KUBECONFIG'], client_configuration: config)
 client = Kubernetes::CoreV1Api.new(Kubernetes::ApiClient.new(config))
 
-pp client.list_namespaced_pod('doridoridoriand')
+pp client.list_namespaced_pod('default')
